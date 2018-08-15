769303e1da003fb52d2c885d6f3065e29d9f953f
Jorge Sagastume <HorgeJ@users.noreply.github.com>
2018-08-15 12:54:38 -0700

    added directory to serve html files and images

--- helloWorld.js
+++ helloWorld.js
@@ -1,20 +1,36 @@
 var http = require('http');
+var fs =   require('fs');
+
+function serveStaticFile(res, path, contentType, responseCode){
+    if(!responseCode){
+        responseCode = 200;
+    }
+    fs.readFile(__dirname + path, function(err,data){
+        if(err){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('500 - internal error');
+        } else {
+            res.writeHead(responseCode, {'Content-Type': contentType});
+            res.end(data);
+        }
+    });
+}
 
 http.createServer(function(req,res){
     // Normalize URL bu removing querystring, optional
     // trailing slash, and making it lowercase
     var path = req.url.replace(/\/?(?:\?.*)?$/,'').toLowerCase(); // replace any char. in url and make string lowercase
     switch(path){
-        case '':    res.writeHead(200,{'Content-Type': 'text/plain'});
-                    res.end('Homepage');
+        case '':    serveStaticFile(res, 'public/home.html', 'text/html');
                     break;
         case '/about':
-                    res.writeHead(200,{'Content-Type': 'text/plain'});
-                    res.end('About');
+                    serveStaticFile(res, 'public/about.html', 'text/html');
                     break;
-        default:    res.writeHead(404,{'Content-Type': 'text/plain'});
-                    res.end('Not Found');
+        case '/img/logo.jpg':
+                    serveStaticFile(res, 'public/img/logo.jpg', 'image/jpeg');
                     break;
+        default:    serveStaticFile(res, 'public/404.html', 'text/html');
+                    break;  
     }
 
 }).listen(3000);
