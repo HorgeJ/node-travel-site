fcd734513983bc74bde1b9585d26c453ab15d400
Jorge Sagastume <HorgeJ@users.noreply.github.com>
2018-08-16 13:37:15 -0700

    added views and layouts and updated routes with static and dynamic content

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 var express = require('express');
 var app = express();
 
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express-handlebars')
+	.create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Silver State Travel');
+app.use(express.static(__dirname + '/public'));
+
+//Travel quotes array
+var quotes = [
+    "What makes the desert beautiful is that somewhere it hides a well.",
+    "A desert is a place without expectation.",
+    "Night comes to the desert all at once, as if someone turned off all the light."
+];
+
+// HOME route
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
+// About route
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Silver State Travel');
+    var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.render('about', {quotes: randomQuote});
 });
 
-// 404 Page
-app.use(function(req, res){
-    res.type('text/plain');
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 // 500 Page
 app.use(function(req, res){
-    res.type('text/plain');
+    console.error(err.stack);
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
